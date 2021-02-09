4aa0840c660989610894e404ba7cc54e50ebac8e
veera_1505 <veerasaraha@gmail.com>
2021-02-09 13:12:50 +0530

    learned fs module using async way

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,8 +1,22 @@
 import fs from "fs"
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
+//Blocking , synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
+// const textOut = `this is what we know about avocado: ${textIn}. \n created on ${new Date()}`
+// fs.writeFileSync("./txt/output.txt", textOut)
+// console.log("File written!")
 
-const textOut = `this is what we know about avocado: ${textIn}. \n created on ${new Date()}`
+// Non-Blocking , asynchronous way
 
-fs.writeFileSync("./txt/output.txt", textOut)
-console.log("File written!")
+fs.readFile("./txt/start.txt", { encoding: "utf-8" }, (err, data) => {
+  fs.readFile(`./txt/${data}.txt`, { encoding: "utf-8" }, (err, data2) => {
+    fs.readFile("./txt/append.txt", { encoding: "utf-8" }, (err, data3) => {
+      if (err) return console.log("EROR : check the file name")
+      console.log(data3)
+      fs.writeFile("./txt/final.txt", `${data2} \n ${data3}`, (err) => {
+        console.log("your fle has been written!")
+      })
+    })
+  })
+})
+console.log("Reading file int the backgroud...")
