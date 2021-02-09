2f20ccef6ca7ce7888f4ad923db9eea57107f729
veera_1505 <veerasaraha@gmail.com>
2021-02-09 13:33:37 +0530

    created simple server using http module

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,13 +1,23 @@
 import fs from "fs"
+import http, { Server } from "http"
+import { Http2ServerResponse } from "http2"
+
+/////////////////////////////////////////////////////////
+// FS MODULE //
+/////////////////////////////////////////////////////////
 
 //Blocking , synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
-// const textOut = `this is what we know about avocado: ${textIn}. \n created on ${new Date()}`
-// fs.writeFileSync("./txt/output.txt", textOut)
-// console.log("File written!")
+/*
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
+const textOut = `this is what we know about avocado: ${textIn}. \n created on ${new Date()}`
+fs.writeFileSync("./txt/output.txt", textOut)
+console.log("File written!")
+
+*/
 
 // Non-Blocking , asynchronous way
 
+/*
 fs.readFile("./txt/start.txt", { encoding: "utf-8" }, (err, data) => {
   fs.readFile(`./txt/${data}.txt`, { encoding: "utf-8" }, (err, data2) => {
     fs.readFile("./txt/append.txt", { encoding: "utf-8" }, (err, data3) => {
@@ -19,4 +29,17 @@ fs.readFile("./txt/start.txt", { encoding: "utf-8" }, (err, data) => {
     })
   })
 })
-console.log("Reading file int the backgroud...")
+console.log("Reading file int the backgroud...") 
+*/
+
+/////////////////////////////////////////////////////////
+// HTTP MODULE //
+/////////////////////////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!!!")
+})
+
+server.listen(3000, () => {
+  console.log("Listening server : PORT 3000")
+})
