d04fd9fb12c0a52262c82950c180c660471aeee9
veera_1505 <veerasaraha@gmail.com>
2021-02-16 10:45:47 +0530

    satrted express

--- asynchronous/index.js
+++ asynchronous/index.js
@@ -10,22 +10,70 @@ const readFilePro = (file) => {
   })
 }
 
+const writePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not writ file")
+      resolve("Sucess")
+    })
+  })
+}
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro("./dog.txt")
+    console.log(`Breed ${data}`)
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    )
+    console.log(res.body.message)
+
+    await writePro("./dog-img.txt", res.body.message)
+    console.log("Random dog image saved to file!")
+  } catch (error) {
+    throw error
+  }
+  return "loaded dog pic...."
+}
+
+;(async () => {
+  try {
+    console.log("1: will get dog pics")
+    const returnValue = await getDogPic()
+    console.log(returnValue)
+    console.log("3 : Done getting dog pics")
+  } catch (error) {
+    console.log(error)
+  }
+})()
+
+/*
+console.log("1: will get dog pics")
+getDogPic()
+  .then((x) => {
+    console.log(x)
+    console.log("3 : Done getting dog pics")
+  })
+  .catch((err) => console.log(err))
+
+ */
+/*
 readFilePro("./dog.txt")
-  .then((data) => {})
-  .catch()
+  .then((data) => {
+    console.log(`Breed ${data}`)
 
-fs.readFile(`./dog.txt`, { encoding: "utf-8" }, (err, data) => {
-  console.log(`BreedL ${data}`)
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+  })
+  .then((res) => {
+    return writePro("./dog-img.txt", res.body.message)
+  })
+  .then(() => {
+    console.log("Random dog image saved to file!")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        if (err) return console.log(err.message)
-        console.log("Random don image saved in file!")
-      })
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-})
+
+  */
---
+++ natours/app.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import { readFileSync, writeFile } from 'fs';
+
+const app = express();
+const PROT = 9090;
+
+//middleware
+app.use(express.json());
+
+const filePath = `${process.cwd()}/natours/devData/data`;
+
+const tours = JSON.parse(readFileSync(`${filePath}/toursSimple.json`, 'utf-8'));
+
+//get all tours data
+app.get('/api/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+//get each tour data
+app.get('/api/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+//post new tour data
+app.post('/api/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  writeFile(`${filePath}/toursSimple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+});
+
+app.listen(PROT, () => {
+  console.log(`App running on port ${PROT}...`);
+});
