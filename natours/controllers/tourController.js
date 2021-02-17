7297d9a06f7eeb60e129784e96b86a2f1bc38a5b
veera_1505 <veerasaraha@gmail.com>
2021-02-17 10:46:29 +0530

    added param middleware function

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -3,6 +3,19 @@ import { readFileSync, writeFile } from 'fs';
 const filePath = `${process.cwd()}/natours/devData/data`;
 const tours = JSON.parse(readFileSync(`${filePath}/toursSimple.json`, 'utf-8'));
 
+//MIDDLEWARE FUNCTIONS
+const checkID = (req, res, next, value) => {
+  const id = Number(value);
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+//HANDLER FUNCTIONS
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,14 +29,9 @@ const getAllTours = (req, res) => {
 
 const getTour = (req, res) => {
   const id = req.params.id * 1;
+
   const tour = tours.find((el) => el.id === id);
 
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -49,14 +57,6 @@ const createTour = (req, res) => {
 };
 
 const updateTour = (req, res) => {
-  const id = req.params.id;
-  if (id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -66,19 +66,10 @@ const updateTour = (req, res) => {
 };
 
 const deleteTour = (req, res) => {
-  const id = req.params.id;
-
-  if (id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
   });
 };
 
-export { getAllTours, createTour, getTour, updateTour, deleteTour };
+export { getAllTours, createTour, getTour, updateTour, deleteTour, checkID };
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { getAllTours, createTour, getTour, updateTour, deleteTour } from './../controllers/tourController.js';
+import { getAllTours, createTour, getTour, updateTour, deleteTour, checkID } from './../controllers/tourController.js';
 
 const tourRouter = express.Router();
+
+tourRouter.param('id', checkID);
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
