eb739f38c95d1239ac1d8052111da3dcac4b0625
veera_1505 <veerasaraha@gmail.com>
2021-02-22 13:23:54 +0530

    added getTour controller

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,28 +1,39 @@
 import Tour from '../models/tourModel.js';
 
 //HANDLER FUNCTIONS
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  // const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+const getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 const createTour = async (req, res) => {
@@ -38,7 +49,7 @@ const createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: error,
+      message: 'Invalid data',
     });
   }
 };
