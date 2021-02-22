b1a0c9c1ef71b3a78357f076294a98d899e24928
veera_1505 <veerasaraha@gmail.com>
2021-02-22 20:40:47 +0530

    created tour model

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'tour must have a durations'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now().toLocaleString(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
