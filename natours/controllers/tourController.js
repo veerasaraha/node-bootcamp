4c276f0986dcd6672427707621c7251f3cb2dcc9
veera_1505 <veerasaraha@gmail.com>
2021-02-23 12:07:09 +0530

    added sorting functionality

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -12,10 +12,17 @@ const getAllTours = async (req, res) => {
     let queryString = JSON.stringify(queryObject);
     queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryString));
+    let query = Tour.find(JSON.parse(queryString));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    }
 
     //EXECUTE QUERY
     const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
