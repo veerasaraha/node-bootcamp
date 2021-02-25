300f1f901b99c3713e00a6d936eee3006630fcea
veera_1505 <veerasaraha@gmail.com>
2021-02-25 11:52:58 +0530

    added data validation in tour model

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 
-const tourSchema = new mongoose.Schema(
+const { Schema } = mongoose;
+
+const tourSchema = new Schema(
   {
     name: {
       type: String,
       required: [true, 'tour must have a name'],
       unique: true,
       trim: true,
+      minlength: [10, 'tour name must have more or equal hen 10 characters'],
+      maxlength: [40, 'tour name must have more or equal hen 10 characters'],
     },
     duration: {
       type: Number,
@@ -19,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either : easy , medium , difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
