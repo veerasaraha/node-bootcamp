f1c92ef76d541770cbe8c89c48e4f05978ec6231
veera_1505 <veerasaraha@gmail.com>
2021-02-26 22:30:11 +0530

    added Error class and refactored old error response methods

--- natours/app.js
+++ natours/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config({ path: `${process.cwd()}/natours/.env` });
 import morgan from 'morgan';
+import AppError from './utils/appError.js';
+import globalErrorHandler from './controllers/errorController.js';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 const app = express();
@@ -25,25 +27,9 @@ app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `can't find ${req.originalUrl} on this server`,
-  // });
-
-  const err = new Error(`can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 export default app;
