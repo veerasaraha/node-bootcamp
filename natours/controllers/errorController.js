4f4bdf85ffa839e88a0c5c33da2196d27af19173
veera_1505 <veerasaraha@gmail.com>
2021-03-01 13:03:50 +0530

    added error response for different environment

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -1,12 +1,39 @@
-export default (err, req, res, next) => {
-  // console.log(err.stack + 'FROM GLOBAL ERROR CLASS');
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendErrorProd = (err, res) => {
+  //operational, trusted error :send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //programming or other unknown error: don't leak error details
+    console.error('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong  ',
+    });
+  }
+};
+
+export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+
   next();
 };
