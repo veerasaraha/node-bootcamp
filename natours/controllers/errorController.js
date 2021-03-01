0fbaf072728e792c4996926c0d5f591a18b25ef3
veera_1505 <veerasaraha@gmail.com>
2021-03-01 13:23:27 +0530

    added handling invalid Database ID Error

--- natours/controllers/errorController.js
+++ natours/controllers/errorController.js
@@ -1,3 +1,10 @@
+import AppError from '../utils/appError.js';
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -32,7 +39,10 @@ export default (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if ((error.name = 'CastError')) error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 
   next();
