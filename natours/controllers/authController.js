2941d3248ddf2da13567ab02e7cbd8ed3118361e
veera_1505 <veerasaraha@gmail.com>
2021-03-02 10:28:57 +0530

    added creating new user route and controller

---
+++ natours/controllers/authController.js
@@ -0,0 +1,14 @@
+import User from './../models/userModel.js';
+import catchAsync from '../utils/cathAsync.js';
+
+const signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+export { signUp };
--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { getAllUsers, createUser, getUser, updateUser, deleteUser } from './../controllers/userController.js';
+import { signUp } from '../controllers/authController.js';
 const userRouter = express.Router();
+
+userRouter.post('/signup', signUp);
 
 userRouter.route('/').get(getAllUsers).post(createUser);
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
