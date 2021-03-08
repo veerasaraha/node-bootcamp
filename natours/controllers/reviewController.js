fdc99124ad747756a213e6df7acc574c40e920f3
veera_1505 <veerasaraha@gmail.com>
2021-03-08 14:27:54 +0530

    added nested GET endpoint

--- natours/controllers/reviewController.js
+++ natours/controllers/reviewController.js
@@ -17,7 +17,9 @@ const createReview = catchAsync(async (req, res, next) => {
 });
 
 const getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
--- natours/routes/reviewRoute.js
+++ natours/routes/reviewRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getAllReviews, createReview } from './../controllers/reviewController.js';
 import { protectRoute, restrictTo } from './../controllers/authController.js';
+
 const reviewRouter = express.Router({ mergeParams: true });
 
 reviewRouter.route('/').get(getAllReviews).post(protectRoute, restrictTo('user'), createReview);
