eda2d14bfe9357b24dbcf74ffd915515ec31dfb3
veera_1505 <veerasaraha@gmail.com>
2021-03-09 13:26:32 +0530

    added importing review and user data

--- natours/import-dev-data.js
+++ natours/import-dev-data.js
@@ -2,6 +2,9 @@ import { readFileSync } from 'fs';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Tour from './models/tourModel.js';
+import Review from './models/reviewModel.js';
+import User from './models/userModel.js';
+
 dotenv.config();
 
 // DB Connection
@@ -18,10 +21,15 @@ mongoose
 
 // READ json File
 const tours = JSON.parse(readFileSync('./devData/data/tours.json', { encoding: 'utf-8' }));
+const users = JSON.parse(readFileSync('./devData/data/users.json', { encoding: 'utf-8' }));
+const reviews = JSON.parse(readFileSync('./devData/data/reviews.json', { encoding: 'utf-8' }));
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+
     console.log('Data successfully loaded!');
   } catch (error) {
     console.log(error);
@@ -32,6 +40,9 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+
     console.log('Data successfuly deleted!');
   } catch (error) {
     console.log(error);
