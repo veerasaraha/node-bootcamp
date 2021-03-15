480e538b7377420133751a8168cf11211900931d
veera_1505 <veerasaraha@gmail.com>
2021-03-15 13:39:36 +0530

    created tour overview template

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -1,6 +1,12 @@
-const getOverview = (req, res, next) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/cathAsync.js';
+
+const getOverview = catchAsync(async (req, res, next) => {
+  // GET all tour data from collection
+  const tours = await Tour.find();
+
+  res.status(200).render('overview', { title: 'All tours', tours });
+});
 
 const getTour = (req, res, next) => {
   res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
