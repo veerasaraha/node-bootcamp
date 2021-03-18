7b72ee945e1a2000a0e81d700c415a13ed7935b6
veera_1505 <veerasaraha@gmail.com>
2021-03-18 13:30:29 +0530

    added updating user password in front-end

--- natours/public/javascript/index.js
+++ natours/public/javascript/index.js
@@ -1,7 +1,7 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox.js';
 import { login, logout } from './login.js';
-import { updateData } from './updateSettings.js';
+import { updateSettings } from './updateSettings.js';
 
 // DOM ELEMENTS
 const mapbox = document.getElementById('map');
@@ -9,6 +9,7 @@ const loginForm = document.querySelector('.form-login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 
 const userDataFrom = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapbox) {
@@ -33,6 +34,28 @@ if (userDataFrom) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn-save--password').textContent = 'Updating...';
+
+    // get fileds value
+    const currentPassword = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    /// making api call to update password
+    await updateSettings({ currentPassword, password, passwordConfirm }, 'password');
+
+    // clear password fields
+    document.querySelector('.btn-save--password').textContent = 'Save Password';
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- natours/public/javascript/updateSettings.js
+++ natours/public/javascript/updateSettings.js
@@ -1,21 +1,27 @@
 import axios from 'axios';
 import { showAlert } from './alerts.js';
-export const updateData = async (name, email) => {
+
+// type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:9090/api/users/updatePassword'
+        : 'http://127.0.0.1:9090/api/users/updateProfile';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:9090/api/users/updateProfile',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data: data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data Updated successfully!');
+      showAlert('success', `${type.toUpperCase()} Updated successfully!`);
     }
   } catch (err) {
     console.log(err);
     showAlert('error', err.response.data.message);
   }
 };
+
+// /api/users/updatePassword
